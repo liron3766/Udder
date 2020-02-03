64fb9f4daed5107ba30bd41c314cb77578b8e416
Liron Parasol <lironseo@gmail.com>
2020-02-03 09:36:20 -0600

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Liron Parasol on 2/3/20.
+//  Copyright © 2020 Liron Parasol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
